9d6932d218ae49087f64212b2cca7c1c097a9292
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-06-26 06:38:30 +0100

    s4as web

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 h-20 flex items-center z-50 ">
       <div className="absolute inset-0 bg-white bg-opacity-50 backdrop-filter backdrop-blur-md"></div>
-      <Container className="relative py-6 flex justify-between items-center">
+      <Container className="relative py-6 flex justify-between items-center px-2">
         <Logo />
         <Menu />
       </Container>
