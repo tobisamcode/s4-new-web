1997c48103c37991310ea5e29b25875be7b99df0
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-06-26 06:38:37 +0100

    s4as web

--- components/header/menu/Menu.tsx
+++ components/header/menu/Menu.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 const Menu = () => {
   return (
-    <div className="flex items-center gap-6">
-      <Link href="" className="text-grey-100 font-[400] text-sm">
+    <div className="flex items-center gap-2 md:gap-6">
+      <Link href="" className="text-grey-100 font-[400] text-xs md:text-sm">
         Sign In
       </Link>
-      <Button label="Book A Demo" />
+      <Button label="Book A Session" />
     </div>
   );
 };
