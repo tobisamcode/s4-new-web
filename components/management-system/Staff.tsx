ede6136f4d2d41ef6ba113001cd45842ced34e9a
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-06-26 06:38:48 +0100

    s4as web

--- components/management-system/Staff.tsx
+++ components/management-system/Staff.tsx
@@ -17,7 +17,7 @@ const staff: string[] = [
 const float = (duration: number) => ({
   initial: { y: -10 },
   animate: {
-    y: [6, -6],
+    y: [4, -4],
     transition: {
       duration: duration,
       ease: "linear",
@@ -29,11 +29,11 @@ const float = (duration: number) => ({
 
 const Staff = () => {
   return (
-    <div className="grid grid-cols-7 gap-2 ">
+    <div className="grid md:grid-cols-7 space-y-10 gap-2 ">
       <div className="col-span-4 flex items-center ">
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
+          initial={{ opacity: 0, x: -20 }}
           transition={{ duration: 1.5 }}
           className="relative"
         >
@@ -42,14 +42,14 @@ const Staff = () => {
             alt="communication image"
             width={1000}
             height={1000}
-            className="w-[80%] rounded-md "
+            className="md:w-[80%] rounded-md "
           />
           <motion.div
             //@ts-ignore
             variants={float(2.3)}
             initial="initial"
             animate="animate"
-            className="w-[150px] flex justify-center items-center bg-white rounded-[5px] p-2 border-[.5px] border-gray-600 absolute -left-12 -top-12"
+            className="w-[140px] text-sm md:text-base md:w-[150px] flex justify-center items-center bg-white rounded-[5px] p-2 border-[.5px] border-gray-600 absolute -left-2 md:-left-12 -top-12"
           >
             Enhance staff management
           </motion.div>
@@ -58,7 +58,7 @@ const Staff = () => {
             variants={float(2)}
             initial="initial"
             animate="animate"
-            className="w-[130px] flex justify-center items-center bg-white rounded-[5px] p-2 border-[.5px] border-gray-600 absolute right-12 top-[45%]"
+            className="w-[120px] md:w-[130px] text-sm md:text-base flex justify-center items-center bg-white rounded-[5px] p-2 border-[.5px] border-gray-600 absolute -right-3 md:right-12 top-[45%]"
           >
             Maximum admin role
           </motion.div>
@@ -67,7 +67,7 @@ const Staff = () => {
             variants={float(2.7)}
             initial="initial"
             animate="animate"
-            className="w-[170px] flex justify-center items-center bg-white rounded-[5px] p-2 border-[.5px] border-gray-600 absolute -left-12 -bottom-12"
+            className="w-[150px] md:w-[170px] text-sm md:text-base flex justify-center items-center bg-white rounded-[5px] p-2 border-[.5px] border-gray-600 absolute -left-3 md:-left-12 -bottom-12"
           >
             Allows overseeing activities
           </motion.div>
@@ -79,7 +79,7 @@ const Staff = () => {
 
         <motion.p
           whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: 100 }}
+          initial={{ opacity: 0, x: 20 }}
           transition={{ duration: 1.1 }}
           className="text-grey-100 text-base mb-6"
         >
@@ -90,7 +90,7 @@ const Staff = () => {
 
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: 100 }}
+          initial={{ opacity: 0, x: 20 }}
           transition={{ duration: 1.5 }}
           className="flex flex-col space-y-4 pl-1"
         >
