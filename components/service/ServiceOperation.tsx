63790933b98e62838aa3a0c7b411c7ceb993c95c
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-06-26 06:39:05 +0100

    s4as web

--- components/service/ServiceOperation.tsx
+++ components/service/ServiceOperation.tsx
@@ -7,13 +7,13 @@ const service: string[] = ["Private", "Public (Marketplace)", "Both"];
 
 const ServiceOperation = () => {
   return (
-    <div className="grid grid-cols-7 gap-2 ">
+    <div className="grid md:grid-cols-7 space-y-6 gap-2 ">
       <div className="col-span-3 ">
         <SectionHeader label="Service Operation Mode" />
 
         <motion.p
           whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
+          initial={{ opacity: 0, x: -20 }}
           transition={{ duration: 2 }}
           className="text-grey-100 text-base mb-6"
         >
@@ -24,7 +24,7 @@ const ServiceOperation = () => {
 
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
+          initial={{ opacity: 0, x: -20 }}
           transition={{ duration: 2.4 }}
           className="flex flex-col space-y-4 pl-1"
         >
@@ -39,7 +39,7 @@ const ServiceOperation = () => {
 
       <motion.div
         whileInView={{ opacity: 1, x: 0 }}
-        initial={{ opacity: 0, x: 100 }}
+        initial={{ opacity: 0, x: 20 }}
         transition={{ duration: 2.4 }}
         className="col-span-4 flex justify-center items-center"
       >
@@ -48,7 +48,7 @@ const ServiceOperation = () => {
           alt="communication image"
           width={1000}
           height={1000}
-          className="w-[70%] rounded-md"
+          className="md:w-[70%] rounded-md"
         />
       </motion.div>
     </div>
